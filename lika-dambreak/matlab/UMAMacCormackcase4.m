49af606240861117cb73bae9ef19730dad0564ae
taruma <tarumainfo@gmail.com>
2018-03-31 09:45:03 +0700

    edit time info

--- lika-dambreak/matlab/UMAMacCormackcase4.m
+++ lika-dambreak/matlab/UMAMacCormackcase4.m
@@ -1,6 +1,6 @@
 %% INFORMATION! READ ME FIRST
 % Program Saint-Venant: MacCormack Scheme
-% Case: 4 - Tidal Wave 
+% Case: 4 - Tidal Wave
 % Script by Taruma (25017046), hi@taruma.info
 % Documentation: N/A
 % Github this project: https://github.com/taruma/belajar-tsa
@@ -20,7 +20,7 @@ beta = 1;   % Correction
 %% --- Channel (Elevation and Properties)
 % --Water Elevation
 h1 = 2;     % Upstream head in (meter) elevation
-h2 = h1;    % Downstream head in (meter) elevation
+h2 = 1.5;    % Downstream head in (meter) elevation
 
 % --Channel Properties
 lf = 100;   % Length of channel in (meter)
@@ -35,9 +35,9 @@ yit = 10;   % Iteration
 dx = 0.5;   % Grid length
 dt = 0.1;   % Time step
 % -- Time
-time = 100;         % Duration simulation in seconds
+time = 200;         % Duration simulation in seconds
 tone = 1/dt;        % (tone) iteration for one second
-tout = tone*0.1;    % Plotting each 'tout' second(s)
+tout = tone*1;    % Plotting each 'tout' second(s)
 % Grid/Time Step
 imax = lf/dx+1; % Total grid
 tmax = time/dt; % Time Step (iteration)
@@ -72,9 +72,9 @@ val_x = 1:imax;         % Absis value
 
 %% --- Channel and Datum Elevation
 % Input
-chan_up = -0.3;               % Channel elevation at grid 1 (left)
-slope1 = -0.3/lf;                 % '-' mean ascending
-slope2 = 0;                 % 
+chan_up = 0;               % Channel elevation at grid 1 (left)
+slope1 = 0;                 % '-' mean ascending
+slope2 = 0;                 %
 sc = 0.9*lf;    % Slope change at (lc) meter from left
 
 % Channel Elevation
@@ -121,7 +121,7 @@ An = A; % An = Ap1_0
 Qn = Q; % Qn = Qp1_0
 
 %% PROG: INITIAL CONDITION
-% Assign water elevation and Calc. channel prop. 
+% Assign water elevation and Calc. channel prop.
 for i = 1:imax
     if i <= imax/2
         h(i) = h1 - chan_ev(i);
@@ -136,8 +136,9 @@ end
 
 %% PROG: MACCORMACK SCHEME
 % Iteration by time
+tnow = 0;
 for t = 1:tmax
-    tnow = t*dt;                    % Calc. (real) time
+    tnow = tnow + dt;                    % Calc. (real) time
     
     % Start of Predictor Step----
     for i = 2:imax-1
@@ -152,21 +153,21 @@ for t = 1:tmax
         I3p = g * A(i) * ((h(i+1)+z(i+1))-(h(i)+z(i)))/dx;
         I4p = g * Q(i) * abs(Q(i)) * n^2 / (A(i)*R(i)^(4/3));
         % Momentum-Predictor
-        Qp(i) = Q(i) - dt * (I2p + I3p + I4p);        
+        Qp(i) = Q(i) - dt * (I2p + I3p + I4p);
     end
     
     % Boundary Condition-Predictor
     if wall_check % IGNORE THE WARNING MESSAGE
-        Qp(1) = 0; 
+        Qp(1) = 0;
     else
         Qp(1) = Qp(2);
     end
-    Qp(imax) = Qp(imax-1); 
+    Qp(imax) = Qp(imax-1);
     Ap(1) = Ap(2);
     hp(imax) = h2 + tidalwave(tnow, periode, ampli);
-    hp(imax) = hp(imax) - chan_ev(imax); 
+    hp(imax) = hp(imax) - chan_ev(imax);
     Ap(imax) = (b(imax) + m*hp(imax))*hp(imax);
-
+    
     zp = z;
     % Find depth (h) value
     for i = 1:imax-1
@@ -179,30 +180,30 @@ for t = 1:tmax
         % Continuity-Initial
         A(i) = (b(i) + m*h(i))*h(i);
         P(i) = b(i) + 2*h(i)*sqrt(1+m^2);
-        R(i) = A(i)/P(i);        
+        R(i) = A(i)/P(i);
         % Continuity-Corrector
         Aph = (A(i) + Ap(i))/2;
-        An(i) = Aph - dt/(2*dx) * (Qp(i)-Qp(i-1));        
+        An(i) = Aph - dt/(2*dx) * (Qp(i)-Qp(i-1));
         
         % Momentum-Initial
         I2c = beta * ((Qp(i)^2/Ap(i)) - (Qp(i-1)^2/Ap(i-1)))/dx;
         I3c = g * A(i) * ((hp(i)+zp(i))-(hp(i-1)+zp(i-1)))/dx;
-        I4c = g * Q(i) * abs(Q(i)) * n^2 / (A(i)*R(i)^(4/3));  
+        I4c = g * Q(i) * abs(Q(i)) * n^2 / (A(i)*R(i)^(4/3));
         % Momentum-Corrector
         Qph = (Q(i)+Qp(i))/2;
-        Qn(i) = Qph - dt/2 * (I2c + I3c + I4c);        
+        Qn(i) = Qph - dt/2 * (I2c + I3c + I4c);
     end
     
     % Boundary Condition-Corrector/Final
     if wall_check % IGNORE THE WARNING MESSAGE
-        Qn(1) = 0; 
+        Qn(1) = 0;
     else
         Qn(1) = Qn(2);
     end
     
     Qn(imax) = Qn(imax-1);
     An(1) = An(2);
-    h(imax) = h2 + tidalwave(tnow, periode, ampli); 
+    h(imax) = h2 + tidalwave(tnow, periode, ampli);
     h(imax) = h(imax) - chan_ev(imax);
     An(imax) = (b(imax) + m*h(imax))*h(imax);
     % ---End of Corrector Step
@@ -214,7 +215,7 @@ for t = 1:tmax
     % Find depth with Current
     for i = 1:imax-1
         h(i) = secantmet(yawal, dy, yit, [A(i) b(i) m]);
-    end    
+    end
     
     % --- End of Numerical Solution
     
@@ -247,27 +248,27 @@ for t = 1:tmax
         ylabel('Elevasi(m)');
         pause(0.001)
         hold off
-
+        
         subplot(2,1,2);
         p2 = plot(val_x,Q,'green');
         hold on
-        title('Q');
+        title(tnow);
         axis manual
         axis ([1 imax -30 30]);
         xlabel('Grid');
         ylabel('Q (m3/s)');
         pause(0.001)
         hold off
-
+        
     end
 end
 
 %% FUNCTION: PROGRAM
 function h = tidalwave(t, T, A)
-    % t = Time
-    % T = Period
-    % A = Amplitude
-    h = sin(2*pi*t/T)*A;
+% t = Time
+% T = Period
+% A = Amplitude
+h = sin(2*pi*t/T)*A;
 end
 
 function y = secantmet(yawal, dy, yit, Abm)
@@ -276,32 +277,32 @@ function y = secantmet(yawal, dy, yit, Abm)
 %         for j = 1:yit % 5 iterasi
 %             yplus = yawal + dy;
 %             ymin = yawal - dy;
-%             
+%
 %             Fawal = A(i) - b(i)*yawal - m * yawal^2;
 %             Fplus = A(i) - b(i)*yplus - m * yplus^2;
 %             Fmin = A(i) - b(i)*ymin - m * ymin^2;
-%             
+%
 %             dF = (Fplus - Fmin) / (2*dy);
-% 
+%
 %             yfinal = yawal - Fawal/dF;
 %             yawal = yfinal;
-%         end       
+%         end
 %        h(i) = yfinal;
-    A = Abm(1);
-    b = Abm(2);
-    m = Abm(3);
-    for i = 1:yit
-        yplus = yawal + dy;
-        ymin = yawal - dy;
-        
-        Fawal = A - b*yawal - m * yawal^2;
-        Fplus = A - b*yplus - m * yplus^2;
-        Fmin =  A - b*ymin  - m * ymin^2;
-
-        dF = (Fplus - Fmin) / (2*dy);
-
-        yfinal = yawal - Fawal/dF;
-        yawal = yfinal;
-    end
-    y = yfinal; 
+A = Abm(1);
+b = Abm(2);
+m = Abm(3);
+for i = 1:yit
+    yplus = yawal + dy;
+    ymin = yawal - dy;
+    
+    Fawal = A - b*yawal - m * yawal^2;
+    Fplus = A - b*yplus - m * yplus^2;
+    Fmin =  A - b*ymin  - m * ymin^2;
+    
+    dF = (Fplus - Fmin) / (2*dy);
+    
+    yfinal = yawal - Fawal/dF;
+    yawal = yfinal;
+end
+y = yfinal;
 end
