e47e77547a518cd5aceb0c4473c558be709f56be
taruma <tarumainfo@gmail.com>
2018-08-22 19:51:25 +0700

    add new line for info

--- playground-misc/ipynbtopdf.bat
+++ playground-misc/ipynbtopdf.bat
@@ -1,3 +1,8 @@
 @echo off
 
-for %%f in (*.ipynb) do jupyter nbconvert "%%f" --to=pdf --TemplateExporter.exclude_output=True
+for %%f in (*.ipynb) do (
+	echo ========BEGIN: Convert %%f
+	jupyter nbconvert "%%f" --to=pdf --TemplateExporter.exclude_output=True
+	echo ========FINISH: Convert %%f
+	echo 
+)
